2609a9bdd46b2af3133c31faed4fe1b92a349721
DipperCypher <dippergamemakerguide@gmail.com>
2026-09-17 21:55:48 -0400

    feat: admin delete for grave stone

--- src/main/java/dev/dipper/graveStone/menu/ViewMenu.java
+++ src/main/java/dev/dipper/graveStone/menu/ViewMenu.java
@@ -1,6 +1,7 @@
 package dev.dipper.graveStone.menu;
 
 import dev.dipper.graveStone.block.BlockData;
+import dev.dipper.graveStone.block.BlockKey;
 import dev.dipper.graveStone.manager.CorpseManager;
 import dev.nexisApi.gui.GuiManager;
 import dev.nexisApi.menu.PaginatedMenu;
@@ -18,13 +19,13 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ViewMenu extends PaginatedMenu<BlockData> {
-    private final CorpseManager corpseM;
-    private final GuiManager guiM;
+    private final CorpseManager corpseManager;
+    private final GuiManager guiManager;
 
-    public ViewMenu(JavaPlugin plugin, CorpseManager corpseM, GuiManager guiM) {
+    public ViewMenu(JavaPlugin plugin, CorpseManager corpseManager, GuiManager guiManager) {
         super(plugin);
-        this.corpseM = corpseM;
-        this.guiM = guiM;
+        this.corpseManager = corpseManager;
+        this.guiManager = guiManager;
     }
 
     @Override
@@ -39,7 +40,7 @@ public class ViewMenu extends PaginatedMenu<BlockData> {
 
     @Override
     protected CompletableFuture<List<BlockData>> loadDataAsync(Player player) {
-        return CompletableFuture.supplyAsync(() -> new ArrayList<>(corpseM.getDeathChest().values()));
+        return CompletableFuture.supplyAsync(() -> new ArrayList<>(corpseManager.getDeathChest().values()));
     }
 
     @Override
@@ -47,12 +48,43 @@ public class ViewMenu extends PaginatedMenu<BlockData> {
         return makeItem(Material.LODESTONE,
                 ChatColor.AQUA + "Player: " + item.getName(),
                 ChatColor.YELLOW + "Created: " + formatTime(item.getCreatTime()),
-                ChatColor.GRAY + item.getKey().toString());
+                ChatColor.GRAY + item.getKey().toString(),
+                ChatColor.RED + "Shift click to Delete");
     }
 
     @Override
-    protected void onElementClick(Player player, BlockData item, InventoryClickEvent event) {
-         guiM.openMenuandLoad(player, new GraveMenu(plugin, corpseM, item, false));
+    protected void onElementClick(Player player, BlockData data, InventoryClickEvent event) {
+        if (event.isShiftClick()) {
+            corpseManager.remove(data, data.getKey());
+            player.sendMessage(ChatColor.RED + "Deleted: " + data.getKey());
+            removeStone(data);
+            requestReload();
+            return;
+        }
+
+         guiManager.openMenuandLoad(player, new GraveMenu(plugin, corpseManager, data, false));
+    }
+
+    private void removeStone(BlockData data) {
+        BlockKey key = data.getKey();
+        World world = Bukkit.getWorld(key.world());
+        if (world == null) {
+            return;
+        }
+
+        Location location = new Location(
+                world,
+                key.x(),
+                key.y(),
+                key.z()
+        );
+
+       location.getBlock().setType(Material.AIR);
+       Location above = location.clone().add(0, 1, 0);
+
+       if (above.getBlock().getType() == corpseManager.getChestBlock()) {
+           above.getBlock().setType(Material.AIR);
+       }
     }
 
     private final DateTimeFormatter TIME_FORMAT =
